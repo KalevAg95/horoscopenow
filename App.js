30446958b3099a29d0de35506dba0e5fb0c98f30
HebertCordero <heberto.cordero@outlook.com>
2019-05-28 19:36:31 -0500

    touchable buttons and color update

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, TouchableHighlight, Alert} from 'react-native';
 import { Header, ThemeProvider } from 'react-native-elements';
 import { FlatGrid } from 'react-native-super-grid';
 
@@ -20,52 +20,63 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
+  _onLongPressButton() {
+    Alert.alert('You long-pressed the button!')
+  }
   render() {
     const items = [
-      { name: 'PISCES', color: '#80CDD4', text: 'February 19 - March 20', img: require('./res/horos_icons/pisces-fishes-shapes-sign.png') },
-      { name: 'AQUARIUS', color: '#80A8CF', text: 'January 20 - February 18', img: require('./res/horos_icons/aquarius-zodiac-sign-symbol-1.png') },
-      { name: 'CAPRICORN', color: '#B88FC0', text: 'December 22 - January 19', img: require('./res/horos_icons/capricorn.png') },
-      { name: 'SAGITTARIUS', color: '#F38388', text: 'November 22 - December 21', img: require('./res/horos_icons/sagittarius-sign-of-an-archer.png') },
-      { name: 'SCORPIO', color: '#80CAA0', text: 'October 23 - November 21', img: require('./res/horos_icons/scorpion-shape.png') },
-      { name: 'LIBRA', color: '#7FBCE1', text: 'September 23 - October 22', img: require('./res/horos_icons/libra-balanced-scale-zodiac-symbol.png') },
-      { name: 'VIRGO', color: '#E487C0', text: 'August 23 - September 22', img: require('./res/horos_icons/virgo-female-silhouette.png') },
-      { name: 'LEO', color: '#E1A490', text: 'July 23 - August 22', img: require('./res/horos_icons/leo-lion-astrological-symbol.png') },
-      { name: 'CANCER', color: '#CADD8F', text: 'June 21 - July 22', img: require('./res/horos_icons/cancer-1.png') },
-      { name: 'GEMINI', color: '#FEE980', text: 'May 21 - June 20', img: require('./res/horos_icons/gemini-twins-symbol.png') },
-      { name: 'TAURUS', color: '#FDDC7F', text: 'April 20 - May 20', img: require('./res/horos_icons/taurus-bull-head-symbol-for-zodiac.png') },
-      { name: 'ARIES', color: '#F8BE80', text: 'March 21 - April 19', img: require('./res/horos_icons/aries-zodiac-symbol-of-frontal-goat-head.png') }
+      { name: 'PISCES', color: '#607D8B', color2: '#B0BEC5', text: 'Feb 19 - Mar 20', img: require('./res/horos_icons/pisces-fishes-shapes-sign.png') },
+      { name: 'AQUARIUS', color: '#78909C', color2: '#B0BEC5', text: 'Jan 20 - Feb 18', img: require('./res/horos_icons/aquarius-zodiac-sign-symbol-1.png') },
+      { name: 'CAPRICORN', color: '#607D8B', color2: '#B0BEC5', text: 'Dec 22 - Jan 19', img: require('./res/horos_icons/capricorn.png') },
+      { name: 'SAGITTARIUS', color: '#78909C', color2: '#B0BEC5', text: 'Nov 22 - Dec 21', img: require('./res/horos_icons/sagittarius-sign-of-an-archer.png') },
+      { name: 'SCORPIO', color: '#607D8B', color2: '#B0BEC5', text: 'Oct 23 - Nov 21', img: require('./res/horos_icons/scorpion-shape.png') },
+      { name: 'LIBRA', color: '#78909C', color2: '#B0BEC5', text: 'Sep 23 - Oct 22', img: require('./res/horos_icons/libra-scale-balance-symbol.png') },
+      { name: 'VIRGO', color: '#607D8B', color2: '#B0BEC5', text: 'Aug 23 - Sep 22', img: require('./res/horos_icons/virgo-female-silhouette.png') },
+      { name: 'LEO', color: '#78909C', color2: '#B0BEC5', text: 'Jul 23 - Aug 22', img: require('./res/horos_icons/leo-astrological-sign.png') },
+      { name: 'CANCER', color: '#607D8B', color2: '#B0BEC5', text: 'Jun 21 - Jul 22', img: require('./res/horos_icons/cancer-1.png') },
+      { name: 'GEMINI', color: '#78909C', color2: '#B0BEC5', text: 'May 21 - Jun 20', img: require('./res/horos_icons/gemini-twins-symbol.png') },
+      { name: 'TAURUS', color: '#607D8B', color2: '#B0BEC5', text: 'Apr 20 - May 20', img: require('./res/horos_icons/taurus-bull-head-symbol-for-zodiac.png') },
+      { name: 'ARIES', color: '#78909C', color2: '#B0BEC5', text: 'Mar 21 - Apr 19', img: require('./res/horos_icons/aries-zodiac-symbol-of-frontal-goat-head.png') }
     ];
     return (
       <ThemeProvider>
       <Header
         statusBarProps={{ barStyle: 'dark-content' }}
         barStyle="dark-content" // or directly
-        centerComponent={{ text: 'HOROSCOPE', style: { color: '#4C566A', fontSize: 25, } }}
+        centerComponent={{ text: 'HOROSCOPE', style: { color: '#CFD8DC', fontSize: 28, } }}
         containerStyle={{
-          backgroundColor: '#141516',
+          backgroundColor: '#455A64',
           justifyContent: 'space-around',
         }}
       />
-
       <View style={styles.container}>
         <FlatGrid
-          itemDimension={130}
+          itemDimension={137}
           items={items}
           style={styles.gridView}
+          spacing={0}
           // staticDimension={300}
           // fixed
           // spacing={20}
           renderItem={({ item, index }) => (
+            <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
             <View style={[styles.itemContainer, { backgroundColor: item.color }]}>
-              <Image source={item.img} style={{width: 50, height: 50}} />
+              <Image source={item.img} style={{width: 50, height: 50, tintColor:item.color2}} />
               <Text style={styles.itemName}>{item.name}</Text>
               <Text style={styles.itemCode}>{item.text}</Text>
             </View>
+            </TouchableHighlight>
           )}
         />
+        {/*
         <Text style={styles.welcome}>Tap on your Sign to get Started</Text>
         <Text style={styles.instructions}>Horoscopes are updated daily</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        */}
       </View>
       </ThemeProvider>
     );
@@ -77,7 +88,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#546E79',
   },
   welcome: {
     fontSize: 20,
@@ -90,24 +101,24 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   gridView: {
-    marginTop: 20,
+    marginTop: 0,
     flex: 1,
   },
   itemContainer: {
     justifyContent: 'flex-end',
-    borderRadius: 5,
-    padding: 10,
+    borderRadius: 0,
     height: 150,
+    padding: 10,
   },
   itemName: {
-    fontSize: 16,
-    color: '#fff',
+    fontSize: 14,
+    color: '#CFD8DC',
     fontWeight: '600',
   },
   itemCode: {
     fontWeight: '600',
-    fontSize: 12,
-    color: '#fff',
+    fontSize: 10,
+    color: '#CFD8DC',
   },
   sectionHeader: {
     flex: 1,
