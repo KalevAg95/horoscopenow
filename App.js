6cde24a3a11e71e7e088f21c0117cb05611200a5
HebertCordero <heberto.cordero@outlook.com>
2019-05-29 09:33:49 -0500

    color update

--- App.js
+++ App.js
@@ -14,25 +14,25 @@
  import { createAppContainer, createStackNavigator } from 'react-navigation';
  
  const instructions = Platform.select({
-   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+   ios: 'IOA,\n' + 'AD',
    android:
-     'You can also read you weekly,\n' +
-     'and yearly Horscopes',
+     'ANDROID,\n' +
+     'AD',
  });
  
  const items = [
-    { name: 'PISCES', color: '#607D8B', color2: '#B0BEC5', text: 'Feb 19 - Mar 20', img: require('./res/horos_icons/pisces-fishes-shapes-sign.png') },
-    { name: 'AQUARIUS', color: '#78909C', color2: '#B0BEC5', text: 'Jan 20 - Feb 18', img: require('./res/horos_icons/aquarius-zodiac-sign-symbol-1.png') },
-    { name: 'CAPRICORN', color: '#607D8B', color2: '#B0BEC5', text: 'Dec 22 - Jan 19', img: require('./res/horos_icons/capricorn.png') },
-    { name: 'SAGITTARIUS', color: '#78909C', color2: '#B0BEC5', text: 'Nov 22 - Dec 21', img: require('./res/horos_icons/sagittarius-sign-of-an-archer.png') },
-    { name: 'SCORPIO', color: '#607D8B', color2: '#B0BEC5', text: 'Oct 23 - Nov 21', img: require('./res/horos_icons/scorpion-shape.png') },
-    { name: 'LIBRA', color: '#78909C', color2: '#B0BEC5', text: 'Sep 23 - Oct 22', img: require('./res/horos_icons/libra-scale-balance-symbol.png') },
-    { name: 'VIRGO', color: '#607D8B', color2: '#B0BEC5', text: 'Aug 23 - Sep 22', img: require('./res/horos_icons/virgo-female-silhouette.png') },
-    { name: 'LEO', color: '#78909C', color2: '#B0BEC5', text: 'Jul 23 - Aug 22', img: require('./res/horos_icons/leo-astrological-sign.png') },
-    { name: 'CANCER', color: '#607D8B', color2: '#B0BEC5', text: 'Jun 21 - Jul 22', img: require('./res/horos_icons/cancer-1.png') },
-    { name: 'GEMINI', color: '#78909C', color2: '#B0BEC5', text: 'May 21 - Jun 20', img: require('./res/horos_icons/gemini-twins-symbol.png') },
-    { name: 'TAURUS', color: '#607D8B', color2: '#B0BEC5', text: 'Apr 20 - May 20', img: require('./res/horos_icons/taurus-bull-head-symbol-for-zodiac.png') },
-    { name: 'ARIES', color: '#78909C', color2: '#B0BEC5', text: 'Mar 21 - Apr 19', img: require('./res/horos_icons/aries-zodiac-symbol-of-frontal-goat-head.png') }
+    { name: 'PISCES', color: '#80cdd4', color2: '#e5f5f6', text: 'Feb 19 - Mar 20', img: require('./res/horos_icons/pisces-fishes-shapes-sign.png') },
+    { name: 'AQUARIUS', color: '#80a8cf', color2: '#e5edf5', text: 'Jan 20 - Feb 18', img: require('./res/horos_icons/aquarius-zodiac-sign-symbol-1.png') },
+    { name: 'CAPRICORN', color: '#b88fc0', color2: '#f0e8f2', text: 'Dec 22 - Jan 19', img: require('./res/horos_icons/capricorn.png') },
+    { name: 'SAGITTARIUS', color: '#f18486', color2: '#fce6e6', text: 'Nov 22 - Dec 21', img: require('./res/horos_icons/sagittarius-sign-of-an-archer.png') },
+    { name: 'SCORPIO', color: '#81cba1', color2: '#e5f4ec', text: 'Oct 23 - Nov 21', img: require('./res/horos_icons/scorpion-shape.png') },
+    { name: 'LIBRA', color: '#7fbce1', color2: '#e5f1f9', text: 'Sep 23 - Oct 22', img: require('./res/horos_icons/libra-scale-balance-symbol.png') },
+    { name: 'VIRGO', color: '#dd87bf', color2: '#f8e7f2', text: 'Aug 23 - Sep 22', img: require('./res/horos_icons/virgo-female-silhouette.png') },
+    { name: 'LEO', color: '#f4a687', color2: '#fcede7', text: 'Jul 23 - Aug 22', img: require('./res/horos_icons/leo-astrological-sign.png') },
+    { name: 'CANCER', color: '#cadd8f', color2: '#f4f8e8', text: 'Jun 21 - Jul 22', img: require('./res/horos_icons/cancer-1.png') },
+    { name: 'GEMINI', color: '#fee980', color2: '#fefae5', text: 'May 21 - Jun 20', img: require('./res/horos_icons/gemini-two-twins-heads-symbol.png') },
+    { name: 'TAURUS', color: '#fddc7f', color2: '#fef8e5', text: 'Apr 20 - May 20', img: require('./res/horos_icons/taurus-bull-head-symbol-for-zodiac.png') },
+    { name: 'ARIES', color: '#f8be80', color2: '#fdf2e5', text: 'Mar 21 - Apr 19', img: require('./res/horos_icons/aries-zodiac-symbol-of-frontal-goat-head.png') }
   ];
  class App extends Component{
     static navigationOptions = {
@@ -68,18 +68,16 @@
              onPress={()=> this.props.navigation.navigate('Sign', {currentSign: item})} underlayColor="white">
              <View style={[styles.itemContainer, { backgroundColor: item.color }]}>
                <Image source={item.img} style={{width: 50, height: 50, tintColor:item.color2}} />
-               <Text style={styles.itemName}>{item.name}</Text>
-               <Text style={styles.itemCode}>{item.text}</Text>
+               <Text style={[styles.itemName, {color:item.color2}]}>{item.name}</Text>
+               <Text style={[styles.itemCode, {color:item.color2}]}>{item.text}</Text>
                {/*<Button onPress={()=> this.props.navigation.navigate('Sign')} title="To Sign"></Button>*/}
              </View>
              </TouchableHighlight>
            )}
          />
-         {/*
-         <Text style={styles.welcome}>Tap on your Sign to get Started</Text>
-         <Text style={styles.instructions}>Horoscopes are updated daily</Text>
+         <Text style={styles.welcome}>Fucking Amazing AD</Text>
+         <Text style={styles.instructions}>HERE</Text>
          <Text style={styles.instructions}>{instructions}</Text>
-         */}
        </View>
        </ThemeProvider>
      );
@@ -169,16 +167,18 @@
      flex: 1,
      justifyContent: 'center',
      alignItems: 'center',
-     backgroundColor: '#546E79',
+     backgroundColor: '#ffffff',
+     marginTop: 0
    },
    welcome: {
      fontSize: 20,
      textAlign: 'center',
      margin: 10,
+     color: '#455A64'
    },
    instructions: {
      textAlign: 'center',
-     color: '#333333',
+     color: '#455A64',
      marginBottom: 5,
    },
    gridView: {
@@ -193,13 +193,11 @@
    },
    itemName: {
      fontSize: 14,
-     color: '#CFD8DC',
      fontWeight: '600',
    },
    itemCode: {
      fontWeight: '600',
      fontSize: 10,
-     color: '#CFD8DC',
    },
    sectionHeader: {
      flex: 1,
